ecdca0457351a99ddbea7193b8679595afdf6358
Akniet <kuluev877@gmail.com>
2022-11-14 14:29:10 +0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {DB_CONFIG} from "./utils/db_config";
+
+
+@Module({
+  imports: [
+      TypeOrmModule.forRoot(DB_CONFIG),
+    
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
